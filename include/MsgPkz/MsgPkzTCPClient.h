0af5ade1c9b115ad6eab19c6f87f13074aa523b4
Felix Laufer <felix.laufer@gmail.com>
2022-10-08 10:42:02 +0200

    Update MsgPkzTCPClient.h

--- include/MsgPkz/MsgPkzTCPClient.h
+++ include/MsgPkz/MsgPkzTCPClient.h
@@ -1,7 +1,7 @@
 #ifndef ARDUINO
 
-#ifndef _MSG_PKZ_TCP_CLIENT_H_
-#define _MSG_PKZ_TCP_CLIENT_H_
+#ifndef _SEC_TCP_CLIENT_H_
+#define _SEC_TCP_CLIENT_H_
 
 #include "packetizing/Packetizer.h"
 #include "networking/TCPClient.h"
@@ -9,7 +9,7 @@
 class MsgPkzTCPClient : protected TCPClient, protected Packetizer
 {
 public:
-  MsgPkzTCPClient(const std::string address, const uint16_t port)
+  MsgPkzTCPClient(const std::string& address, const uint16_t port)
     : TCPClient(address, port)
     , Packetizer()
   {}
@@ -19,54 +19,40 @@ public:
   template <typename... TMessages>
   void sendWait(TMessages&&... messages)
   {
-    writeWait(packetize(std::forward<TMessages>(messages)...));
+    TCPClient::sendWait(packetize(std::forward<TMessages>(messages)...));
   }
 
   template <typename... TMessages>
   void sendAsync(TMessages&&... messages)
   {
-    writeAsync(packetize(std::forward<TMessages>(messages)...));
+    TCPClient::sendAsync(std::forward<TMessages>(messages)...);
   }
 
   template <typename... TMessageCallbacks>
   void receiveWait(TMessageCallbacks&&... messageCallbacks)
   {
-    readUntilWait
-    (
-      [&](ByteStream& ps)
-      {
-        depacketize(ps, std::forward<TMessageCallbacks>(messageCallbacks)...);
-      }
-      , PACKET_DELIMITER, false
-    );
+    TCPClient::receiveWait([&](ByteStream& ps)
+    {
+      depacketize(ps, std::forward<TMessageCallbacks>(messageCallbacks)...);
+    }, PACKET_DELIMITER);
   }
 
   template <typename... TMessageCallbacks>
   void receiveAsync(TMessageCallbacks&&... messageCallbacks)
   {
-    readUntilAsync
-    (
-      *this,
-      [&](decltype(*this)&& self, ByteStream& ps)
-      {
-        self.depacketize(ps, std::forward<TMessageCallbacks>(messageCallbacks)...);
-      }
-      , PACKET_DELIMITER, false
-    );
+    TCPClient::receiveAsync([&](ByteStream& ps)
+    {
+      depacketize(ps, std::forward<TMessageCallbacks>(messageCallbacks)...);
+    }, PACKET_DELIMITER);
   }
 
   template <typename... TMessageCallbacks>
   void subscribe(TMessageCallbacks&&... messageCallbacks)
   {
-    readUntilAsync
-    (
-      *this,
-      [&](decltype(*this)&& self, ByteStream& ps)
-      {
-        self.depacketize(ps, std::forward<TMessageCallbacks>(messageCallbacks)...);
-      }
-      , PACKET_DELIMITER, true
-    );
+    TCPClient::subscribe([&](ByteStream& ps)
+    {
+      depacketize(ps, std::forward<TMessageCallbacks>(messageCallbacks)...);
+    }, PACKET_DELIMITER);
   }
 };
 
